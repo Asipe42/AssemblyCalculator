edc8ea89206ebbf84903bfa292028f22a689c1a7
Modin <eom3658@gmail.com>
2025-08-03 18:14:40 +0900

    feat: 사용자 입력 수신

--- AssemblyCalculator/main.asm
+++ AssemblyCalculator/main.asm
@@ -1,13 +1,24 @@
 INCLUDE Irvine32.inc
 .data
-    prompt BYTE "Enter number: ",0
+    prompt0 BYTE "Enter the first number: ",0
+    prompt1 BYTE "Enter the operator (+, -, *, /): ",0
+    prompt2 BYTE "Enter the second number: ",0
+
 .code
 main PROC
     call Clrscr
-    mov edx, OFFSET prompt
+    mov edx, OFFSET prompt0
     call WriteString
     call ReadInt
-    call WriteInt
+
+    mov edx, OFFSET prompt1
+    call WriteString
+    call ReadChar
+
+    mov edx, OFFSET prompt2
+    call WriteString
+    call ReadInt
+
     call Crlf
     exit
 main ENDP
